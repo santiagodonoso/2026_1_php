d93da9fabb766567acf7992d35a181497e764d67
Santiago <santiagodonoso@gmail.com>
2026-02-17 14:39:31 +0100

    .

--- _header.php
+++ _header.php
@@ -7,6 +7,8 @@ require_once __DIR__."/_.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <script src="app.js" defer></script>
     <title>
         <?php _($title ?? "Company") ?>
     </title>
