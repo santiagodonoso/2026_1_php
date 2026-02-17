06bb64c4ee90eb6b0e12be3328f1a1dea4973be7
Santiago <santiagodonoso@gmail.com>
2026-02-17 14:53:07 +0100

    .

--- _header.php
+++ _header.php
@@ -8,6 +8,13 @@ require_once __DIR__."/_.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="app.css">
+
+    <link 
+        rel="stylesheet" 
+        href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    />
+
+
     <script src="app.js" defer></script>
     <title>
         <?php _($title ?? "Company") ?>
--- _main.php
+++ _main.php
@@ -4,6 +4,25 @@
         map
     </div>
 
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+    <script>
+        // Initialize the map
+        const map = L.map('map').setView([51.505, -0.09], 13);
+
+        // Add OpenStreetMap tiles
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
+
+        // Add a marker
+        const marker = L.marker([51.505, -0.09]).addTo(map);
+        marker.bindPopup("<b>Hello!</b><br>This is a Leaflet map.").openPopup();
+    </script>
+
+
+
+
     <aside>
         property
     </aside>
