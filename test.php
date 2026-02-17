dd475c93e0cf2d22e15d995d297ef0ccd10643d0
Santiago <santiagodonoso@gmail.com>
2026-02-17 13:39:47 +0100

    .

--- test.php
+++ test.php
@@ -1,5 +1,41 @@
 <?php
 
-if(str_contains("Email must be at least 6 characters long in ", "user_email")){
-    echo "here";
+require_once __DIR__."/_.php";
+
+// PHP only knows about assosiative arrays =>
+// [  {id:1, name:a}   {id:2, name:b} ]
+$letters = [
+    ["id"=>"1", "name"=>"A"],
+    ["id"=>"2", "name"=>"B"],
+    ["id"=>"3", "name"=>"C"]
+];
+
+$contentType = $_SERVER['CONTENT_TYPE'] ?? '';
+
+// API JSON
+if(str_contains($contentType, "application/json")){
+    header('Content-Type: application/json');
+    echo json_encode($letters);
+    exit;
 }
+
+// HTML
+foreach( $letters as $letter ){
+    echo "<div>";
+    _($letter["name"]);
+    echo "</div>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
